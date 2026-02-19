a8aebf761e3286e7037b5f1ec5454d25cf924176
Roshan Singh <alcanivorax@users.noreply.github.com>
2026-02-19 23:03:16 +0530

    feat: add types

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,9 +1,18 @@
-export interface FileNode {
-  path: string
-  size: number
-}
-
 export type FilterResult = {
   selected: string[]
   skipped: string[]
+}
+
+export interface Metrics {
+  line: number
+  sizeByte: number
+  tokenCount: number
+}
+
+export interface FileInfo {
+  repoId: string
+  path: string
+  relativePath: string
+  extension: string
+  language: string
 }
